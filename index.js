4427c4f781a14cd34e40eb016b0f128b173320fd
Amirulmath <amirulmath760@gmail.com>
2023-07-30 17:15:18 +0600

    Received API and send to client side

--- index.js
+++ index.js
@@ -7,8 +7,14 @@ const port = process.env.PORT || 5000;
 // middle wares
 app.use(cors());
 
+const products = require("./data/products.json");
+
 app.get("/", (req, res) => {
   res.send("PC Shop server is running");
+});
+
+app.get("/products", (req, res) => {
+  res.send(products);
 });
 
 app.listen(port, () => {
